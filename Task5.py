d3efd93a560af5e97356883d3e866df3eb09c96f
AcdLaptop <psvitaday2@gmail.com>
2023-03-28 19:57:57 +0300

    task 6 ex1

--- Task5.py
+++ Task5.py
@@ -1,11 +1,17 @@
 def Ex1():
-    list={}
+    list={"USA":"New york","UK":"Great britan","Russia":"Mosscow","China":"Ching-Chong","Australia":"Canberra"}
     for i in list.keys():
-        print(i, list[i])
+        print(i,":", list[i])
     inp=input()
     if list.get(inp, -1)==-1:
         print("This item does not exist")
     else:
         print(list[inp])
-    
-    
+    outp=sorted(list.keys())
+    for i in outp:
+        print( i,": ",list[i])
+
+def Ex2():
+    list={1:"", 2:"",3:"",4:"",5:"",8:"",10:""}
+
+Ex1()
